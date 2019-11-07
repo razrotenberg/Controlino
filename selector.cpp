46f491b2edfbb61b8085f41093a45131bda58b99
levosos <razhaleva@gmail.com>
2019-11-08 00:42:55 +0200

    Selectors are now of 16 i/o pins

--- selector.cpp
+++ selector.cpp
@@ -5,21 +5,24 @@
 namespace controlino
 {
 
-Selector::Selector(char S0, char S1, char S2) :
+Selector::Selector(char S0, char S1, char S2, char S3) :
     _S0(S0),
     _S1(S1),
-    _S2(S2)
+    _S2(S2),
+    _S3(S3)
 {
     pinMode(_S0, OUTPUT);
     pinMode(_S1, OUTPUT);
     pinMode(_S2, OUTPUT);
+    pinMode(_S3, OUTPUT);
 }
 
 void Selector::select(char pin)
 {
-    digitalWrite(_S0, (pin & 0b001) ? HIGH : LOW);
-    digitalWrite(_S1, (pin & 0b010) ? HIGH : LOW);
-    digitalWrite(_S2, (pin & 0b100) ? HIGH : LOW);
+    digitalWrite(_S0, (pin & 0b0001) ? HIGH : LOW);
+    digitalWrite(_S1, (pin & 0b0010) ? HIGH : LOW);
+    digitalWrite(_S2, (pin & 0b0100) ? HIGH : LOW);
+    digitalWrite(_S3, (pin & 0b1000) ? HIGH : LOW);
 }
     
 } // controlino
--- selector.h
+++ selector.h
@@ -5,14 +5,15 @@ namespace controlino
 
 struct Selector
 {
-    Selector(char S0, char S1, char S2);
+    Selector(char S0, char S1, char S2, char S3);
     
     void select(char pin);
 
 private:
     char _S0; // lsb
     char _S1;
-    char _S2; // msb
+    char _S2;
+    char _S3; // msb
 };
 
 } // controlino
