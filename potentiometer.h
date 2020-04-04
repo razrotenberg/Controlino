5424ea47f50c3724414ae2387780e26b15be5868
levosos <razhaleva@gmail.com>
2020-04-04 16:58:48 +0300

    Templated 'Potentiometer'
    Minimum and maximum allowed values are template arguments
    
    - `check()` returns the new `Event` enum
    - Simplified implementation
    - Removed code duplication

--- potentiometer.h
+++ potentiometer.h
@@ -7,36 +7,39 @@
 namespace controlino
 {
 
+template <int Min, int Max>
 struct Potentiometer : Control
 {
-    Potentiometer(Pin pin);
-    Potentiometer(Multiplexer & multiplexer, Pin pin);
-
-    int check();
-
-    template <int Min, int Max>
-    int check()
+    enum class Event : char
     {
-        int value = -1;
-        (void)check<Min, Max>(/* out */ value);
-        return value;
+        None, // nothing has happened
+
+        Changed,
+    };
+
+    Potentiometer(Pin pin) : Control(pin, Mode::Input),
+        _previous(read())
+    {}
+
+    Potentiometer(Multiplexer & multiplexer, Pin pin) : Control(multiplexer, pin, Mode::Input),
+        _previous(read())
+    {}
+
+    Event check()
+    {
+        auto event = Event::None;
+
+        const auto value = read();
+
+        if (_previous != value)
+        {
+            event = Event::Changed;
+        }
+
+        _previous = value;
+        return event;
     }
 
-    bool check(/* out */ int & value);
-
-    template <int Min, int Max>
-    bool check(/* out */ int & value)
-    {
-        if (!check(/* out */ value)) { return false; }
-
-        /* out */ value = constrain(
-            map(value, 0, 1023, (int)((float)Min * 0.85), (int)((float)Max * 1.15)),
-            Min, Max);
-
-        return true;
-    }
-
-    template <int Min, int Max>
     int read()
     {
         const auto value = analogRead();
