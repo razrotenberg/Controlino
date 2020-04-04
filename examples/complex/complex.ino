f13a11a12717cdfeb2f3b395dbd7b8b6329c72db
levosos <razhaleva@gmail.com>
2020-04-04 21:22:49 +0300

    Build fix in 'complex' example

--- examples/complex/complex.ino
+++ examples/complex/complex.ino
@@ -8,7 +8,7 @@ namespace pin
     constexpr auto S1 = 3;
     constexpr auto S2 = 4;
     constexpr auto S3 = 5;
-    
+
     constexpr auto SIG = A5;
 }; // pin
 
@@ -21,7 +21,7 @@ void loop()
 {
     static Selector __selector(pin::S0, pin::S1, pin::S2, pin::S3);
     static Multiplexer __multiplexer(pin::SIG, __selector);
-        
+
     static Button __buttons[] = {
         { __multiplexer, 0 },
         { 10 },
@@ -52,6 +52,8 @@ void loop()
         else if (event == Key::Event::Up)   { Serial.println("Up");   }
     }
 
+    using Potentiometer = Potentiometer<20, 230>;
+
     static Potentiometer __potentiometers[] = {
         { __multiplexer, 10 },
         { A0 },
@@ -59,11 +61,9 @@ void loop()
 
     for (auto & potentiometer : __potentiometers)
     {
-        const auto value = potentiometer.check<40, 230>();
-
-        if (value != -1)
+        if (potentiometer.check() == Potentiometer::Event::Changed)
         {
-            Serial.println(value);
+            Serial.println(potentiometer.read());
         }
     }
 }
