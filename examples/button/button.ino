770bc684aec00563588890ce0e764863c5c1d763
levosos <razhaleva@gmail.com>
2020-04-04 16:27:51 +0300

    Improvements in 'button' example
    - Supporting muxed button

--- examples/button/button.ino
+++ examples/button/button.ino
@@ -1,15 +1,50 @@
 #include "button.h"
 
-using namespace controlino;
+// `MUXED` should be defined if the button is connected through a multiplexer
+// #define MUXED
 
-void setup()
+namespace controlino
+{
+
+namespace
+{
+namespace pin
+{
+
+#ifdef MUXED
+
+constexpr Pin S0 = 2;
+constexpr Pin S1 = 3;
+constexpr Pin S2 = 4;
+constexpr Pin S3 = 5;
+
+constexpr Pin SIG = 6;
+
+constexpr Pin Button = 0;
+
+#else
+
+constexpr Pin Button = 7;
+
+#endif
+
+}; // pin
+}; //
+
+extern "C" void setup()
 {
     Serial.begin(9600);
 }
 
-void loop()
+extern "C" void loop()
 {
-    static Button __button(2);
+#ifdef MUXED
+    static auto __selector      = Selector(pin::S0, pin::S1, pin::S2, pin::S3);
+    static auto __multiplexer   = Multiplexer(pin::SIG, __selector);
+    static auto __button        = Button(__multiplexer, pin::Button);
+#else
+    static auto __button        = Button(pin::Button);
+#endif
 
     const auto event = __button.check();
 
@@ -20,3 +55,5 @@ void loop()
     else if (event == Button::Event::ClickClick) { Serial.println("ClickClick"); }
     else if (event == Button::Event::ClickPress) { Serial.println("ClickPress"); }
 }
+
+} // controlino
