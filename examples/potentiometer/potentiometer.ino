ae2f2da696e5f445dacd6d2190867f289375865b
levosos <razhaleva@gmail.com>
2020-04-04 17:06:27 +0300

    Improvements in 'potentiometer' example
    - Supporting muxed potentiometer

--- examples/potentiometer/potentiometer.ino
+++ examples/potentiometer/potentiometer.ino
@@ -1,33 +1,57 @@
 #include "potentiometer.h"
 
-using namespace controlino;
+// `MUXED` should be defined if the potentiometer is connected through a multiplexer
+// #define MUXED
 
-void setup()
+namespace controlino
+{
+
+namespace
+{
+namespace pin
+{
+
+#ifdef MUXED
+
+constexpr Pin S0 = 2;
+constexpr Pin S1 = 3;
+constexpr Pin S2 = 4;
+constexpr Pin S3 = 5;
+
+constexpr Pin SIG = A0;
+
+constexpr Pin Potentiometer = 0;
+
+#else
+
+constexpr Pin Potentiometer = A0;
+
+#endif
+
+}; // pin
+}; //
+
+extern "C" void setup()
 {
     Serial.begin(9600);
 }
 
-void loop()
+extern "C" void loop()
 {
-    static Potentiometer __potentiometer(A0);
-    
-    constexpr auto Min = 40;
-    constexpr auto Max = 230;
+    using Potentiometer = Potentiometer<20, 230>;
 
-    if (random(2) == 0) // first API
+#ifdef MUXED
+    static auto __selector      = Selector(pin::S0, pin::S1, pin::S2, pin::S3);
+    static auto __multiplexer   = Multiplexer(pin::SIG, __selector);
+    static auto __potentiometer = Potentiometer(__multiplexer, pin::Potentiometer);
+#else
+    static auto __potentiometer = Potentiometer(pin::Potentiometer);
+#endif
+
+    if (__potentiometer.check() == Potentiometer::Event::Changed)
     {
-        const auto value = __potentiometer.check<Min, Max>();
-        if (value != -1)
-        {
-            Serial.println(value);
-        }
-    }
-    else // second API
-    {
-        int value;
-        if (__potentiometer.check<Min, Max>(/* out */ value))
-        {
-            Serial.println(value);
-        }
+        Serial.println(__potentiometer.read());
     }
 }
+
+} // controlino
